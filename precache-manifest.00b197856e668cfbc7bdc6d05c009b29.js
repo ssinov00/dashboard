e0a5a462170eb9e18fc751eb8314ebe97d3a8b70
Stela Sinovcic <stela@filmweb.no>
2020-03-23 11:29:48 +0100

    Updates

---
+++ precache-manifest.00b197856e668cfbc7bdc6d05c009b29.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1fb3c2ce6619a14e006983390a7e9fa",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "da72741ad5f511b51a30",
+    "url": "/dashboard/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "79174834c324fb488de2",
+    "url": "/dashboard/static/css/main.a1b792a0.chunk.css"
+  },
+  {
+    "revision": "da72741ad5f511b51a30",
+    "url": "/dashboard/static/js/2.c39f24e5.chunk.js"
+  },
+  {
+    "revision": "60cb8bbb629127159ec6eeb4dffb57ad",
+    "url": "/dashboard/static/js/2.c39f24e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79174834c324fb488de2",
+    "url": "/dashboard/static/js/main.a1096e10.chunk.js"
+  },
+  {
+    "revision": "ac1eb9f516ee56a6853f",
+    "url": "/dashboard/static/js/runtime-main.4e4eede7.js"
+  },
+  {
+    "revision": "de17e20e8560a81e1f9ea9eafc8ea34a",
+    "url": "/dashboard/static/media/logo_desktop.de17e20e.svg"
+  },
+  {
+    "revision": "5b79728f9711c110959b19834a878af3",
+    "url": "/dashboard/static/media/logo_mobile.5b79728f.svg"
+  }
+]);
